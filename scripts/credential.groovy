e90f60b89ce4edaf56346b0d90d2dabe1476c7a9
Jinbo Wang <jinbwan@microsoft.com>
2016-09-25 16:38:58 +0800

    Fix

--- scripts/credential.groovy
+++ scripts/credential.groovy
@@ -1,4 +1,5 @@
 def getCredential(credentialId) {
+    /* Try SSH credential */
     def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
         com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey.class,
         jenkins.model.Jenkins.instance
@@ -11,17 +12,28 @@ def getCredential(credentialId) {
         def encodeKey = privateKey.bytes.encodeBase64().toString()
       return "{\"type\":\"SSH\", \"value\":{\"username\": \"${result.getUsername()}\", \"privateKey\": \"${encodeKey}\", \"passphrase\": \"${result.getPassphrase()}\", \"description\": \"${result.getDescription()}\" }}"
     } else {
+        /* Try UsernamePassword credential */
         creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
-        jenkins.model.Jenkins.instance
-        )
+                com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
+                jenkins.model.Jenkins.instance
+            )
         result = creds.findResult { it.id == credentialId ? it : null }
         if (result){
             return "{type:\"Userpass\", value:{username: \"${result.getUsername()}\", password: \"${result.getPassword()}\"}}"
         } else {
-            return "";
+            /* Try Secret credential */
+            creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+                    org.jenkinsci.plugins.plaincredentials.StringCredentials.class,
+                    jenkins.model.Jenkins.instance
+                )
+            result = creds.findResult { it.id == credentialId ? it : null }
+            if (result) {
+                def encodeSecret = result.getSecret().toString().bytes.encodeBase64().toString()
+                return "{type:\"Secret\", value:\"${encodeSecret}\"}"
+            }
         }
     }
+    return "";
 }
 
 println getCredential(credentialId)
---
+++ scripts/setupnpmrc.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var path = require('path');
+var common = require('./common');
+
+var userHome = process.env.USERPROFILE || process.env.HOME;
+
+function saveNpmrc(npmrc) {
+    var npmrcFile = path.join(userHome, '.npmrc');
+    fs.writeFileSync(npmrcFile, npmrc);
+    return npmrcFile;
+}
+
+function setupNpmrc(credentialId) {
+    //var jenkinsUrl = 'https://jinbo:6d716e8c7fdf5607eb331562fc1d3d8d@docsci.cloudapp.net/scriptText';
+    var jenkinsUrl = 'http://admin:test1@10.0.0.4:8080/scriptText';
+    // var jenkinsUrl = common.getJenkinsUrl();
+    var template = fs.readFileSync(path.join(__dirname, 'credential.groovy'));
+    var scriptText = 'def credentialId="' + credentialId + '" \n ' + template;
+    common.executeScript(jenkinsUrl, scriptText, function (error, data) {
+        if (error) {
+            console.log('Setup npmrc failed with error "' + error + '"');
+            process.exit(1);
+        } else if (!data || !data.trim()) {
+            console.log('Cannot find the corresponding credential info via credentialId. Probably it doesn\'t exist.');
+            process.exit(1);
+        } else {
+            var credential = JSON.parse(data);
+            var npmrc =  new Buffer(credential.value, 'base64').toString("ascii");
+            saveNpmrc(npmrc);
+            process.exit(0);
+        }
+    });
+};
+
+if (process.argv.length < 3) {
+    console.log('[Failed] Please specify credentialId parameter.');
+    process.exit(1);
+}
+
+setupssh(process.argv[2]);
