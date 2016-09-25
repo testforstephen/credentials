7745a95ae5cfb2a5494ce60d6032b0341065614e
Jinbo Wang <jinbwan@microsoft.com>
2016-09-25 13:46:00 +0800

    fix

--- scripts/injectEnv.groovy
+++ scripts/injectEnv.groovy
@@ -1,37 +1,10 @@
 
-class VariableInjectionAction implements hudson.model.EnvironmentContributingAction {
-    private String key
-    private String value
-
-    public VariableInjectionAction(String key, String value) {
-        this.key = key
-        this.value = value
-    }
-
-    public void buildEnvVars(hudson.model.AbstractBuild build, hudson.EnvVars envVars) {
-
-        if (envVars != null && key != null && value != null) {
-            envVars.put(key, value);
-        }
-    }
-
-    public String getDisplayName() {
-        return "VariableInjectionAction";
-    }
-
-    public String getIconFileName() {
-        return null;
-    }
-
-    public String getUrlName() {
-        return null;
-    }
-}
-
 def jenkinsInstance = jenkins.model.Jenkins.getInstance()
 def item = jenkins.model.Jenkins.getInstance().getItemByFullName("testJob")
 def currentBuild = item.getBuildByNumber(buildNumber)
-envs.each {
-    currentBuild.addAction(new VariableInjectionAction(it['key'], it['value']))
+def parameters = []
+envs.each{
+    parameters.push(new hudson.model.StringParameterValue(it['key'], it['value']))
 }
+currentBuild.addAction(new hudson.model.ParametersAction(parameters))
 currentBuild.getEnvironment()
